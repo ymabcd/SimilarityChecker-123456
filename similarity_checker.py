87a94ac8fd0c3dc8e00ebbfc8cbbe5a0adedc0b3
ymin86kim <ymin86kim@gmail.com>
2024-06-13 16:46:19 +0900

    [feature] add alpha_comparison

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,2 +1,30 @@
 class SimilarityChecker:
-    pass
+    def alpha_comparison(self, first_string, second_string):
+        if len(first_string) == len(second_string):
+            return 40
+
+        temp = first_string + second_string
+        unique_string = self.get_unique_characters(temp)
+        totalCnt = len(unique_string)
+
+        first_unique_string = self.get_unique_characters(first_string)
+        second_unique_string = self.get_unique_characters(second_string)
+        sameCnt = self.count_common_characters(first_unique_string, second_unique_string)
+
+        return int(sameCnt/totalCnt*40)
+
+    def get_unique_characters(self, string):
+        seen = set()
+        result = []
+        for char in string:
+            if char not in seen:
+                seen.add(char)
+                result.append(char)
+        return result
+
+    def count_common_characters(self, first_unique_string, second_unique_string):
+        common_count = 0
+        for char in first_unique_string:
+            if char in second_unique_string:
+                common_count += 1
+        return common_count
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -1,5 +1,21 @@
 from unittest import TestCase
 
+from similarity_checker import SimilarityChecker
+
 
 class TestSimilarityChecker(TestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarityChecker()
+
+    def test_strings_contain_same_letters(self):
+        self.assertEqual(40, self.checker.alpha_comparison("ASD", "DSA"))
+
+    def test_strings_have_unique_letters(self):
+        self.assertEqual(0, self.checker.alpha_comparison("A", "BB"))
+
+    def test_string_is_all_uppercase(self):
+        self.assertEqual(40, self.checker.alpha_comparison("AAABB", "BAA"))
+
+    def test_strings_contain_letters(self):
+        self.assertEqual(20, self.checker.alpha_comparison("AA","AAE"))
