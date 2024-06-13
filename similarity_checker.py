ee4246910c73b9159a0dd141bb18db1222790541
ymin86kim <ymin86kim@gmail.com>
2024-06-13 16:00:20 +0900

    [feature] check same length, exceeds double

--- similarity_checker.py
+++ similarity_checker.py
@@ -1,2 +1,18 @@
 class SimilarityChecker:
-    pass
+    def compare_same_length(self, first_length, second_length):
+        return first_length == second_length
+
+    def compare_string_exceeds_double(self, first_length, second_length):
+        if self.compare_same_length(first_length, second_length * 2) or self.compare_same_length(second_length,                                                                                             first_length * 2):
+            return True
+        return False
+
+    def string_length_comparison(self, first_string, second_string):
+        first_length = len(first_string)
+        second_length = len(second_string)
+
+        if self.compare_same_length(first_length, second_length):
+            return 60
+
+        if self.compare_string_exceeds_double(first_length, second_length):
+            return 0
--- test_similarity_checker.py
+++ test_similarity_checker.py
@@ -1,5 +1,20 @@
 from unittest import TestCase
 
+from similarity_checker import SimilarityChecker
+
 
 class TestSimilarityChecker(TestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarityChecker()
+
+    def test_strings_have_same_lengths(self):
+        self.assertEqual(60, self.checker.string_length_comparison("ASD", "DSA"))
+
+    def test_strings_have_different_lengths(self):
+        # 긴 문자
+        # 짧은 문자
+        pass
+
+    def test_string_length_difference_exceeds_double(self):
+        self.assertEqual(0, self.checker.string_length_comparison("A", "BB"))
